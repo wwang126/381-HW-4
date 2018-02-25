9c95572f0609a8f4a63f655b7e215568ec76753a
wwang126 <wavelet.wang@gmail.com>
2018-02-24 22:03:27 -0800

    added commments

--- KarelSemantics.hs
+++ KarelSemantics.hs
@@ -27,39 +27,43 @@ stmt PickBeeper _ w r = let p = getPos r
                         in if hasBeeper p w
                               then OK (decBeeper p w) (incBag r)
                               else Error ("No beeper to pick at: " ++ show p)
-stmt Move _ w r = let n = neighbor(getFacing r)(getPos r)
-                  in if test(Clear Front) w r
-                    then OK w (setPos n  r)
+
+stmt Move _ w r = let n = neighbor(getFacing r)(getPos r) -- get neighbor
+                  in if test(Clear Front) w r -- check if there's anything ahead
+                    then OK w (setPos n  r) -- set bot to neighbor postion
                     else Error ("Blocked at: " ++ show n)
 
 stmt PutBeeper _ w r = let p = getPos r
-                        in if isEmpty r
-                            then Error ("No beeper to put.")
-                            else OK (incBeeper p w) (decBag r)
+                        in if isEmpty r -- check bag for beepers
+                            then Error ("No beeper to put.") -- empty bag
+                            else OK (incBeeper p w) (decBag r) -- place beeper
+                            --There can actually be multiple beepers per spot
+                            -- so we don't check for beepers at pos
 stmt (Turn d) _ w r = let c = getFacing r
-                      in if c == cardTurn d c
-                          then Error ("Already facing " ++ show c)
+                      in if c == cardTurn d c -- check for facing
+                          then Error ("Already facing " ++ show c) -- already facing
                           else OK w (setFacing (cardTurn d c) r)
 
-stmt (Block []) d w r = OK w r
-stmt (Block (s:ns)) d w r = case stmt s d w r of
+stmt (Block []) d w r = OK w r -- empty conditional
+stmt (Block (s:ns)) d w r = case stmt s d w r of -- standard loop thru array
                           OK nw nr -> stmt (Block ns) d nw nr
                           otherwise -> otherwise
 
-stmt (If t x y) d w r = case (test t w r) of
+stmt (If t x y) d w r = case (test t w r) of -- case statement to evalute t
                           True -> stmt x d w r
                           False -> stmt y d w r
-stmt (Call m) d w r = case lookup m d of
+stmt (Call m) d w r = case lookup m d of -- Look up macro to perform
                     Nothing -> Error ("Undefined macro: " ++ m)
                     Just s -> stmt s d w r
 
-stmt (While t s) d w r = if test t w r then case stmt s d w r of
-                                            OK nw nr -> stmt (While t s) d nw nr
-                                            otherwise -> otherwise
-                        else OK w r
+stmt (While t s) d w r = if test t w r
+                            then case stmt s d w r of -- same as If basically
+                                 OK nw nr -> stmt (While t s) d nw nr
+                                 otherwise -> otherwise
+                            else OK w r
 stmt (Iterate i s) d w r = let ni = i - 1
                            in case ni of
-                             -1 -> OK w r
+                             -1 -> OK w r -- it's -1 because of indexing errors at 0
                              otherwise -> case stmt s d w r of
                                           OK nw nr -> stmt (Iterate ni s) d nw nr
                                           otherwise -> otherwise
